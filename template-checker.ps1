ad2bcc13098641ac277720fa087c117d100ece57
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-06-28 01:38:13 -0700

    validating the .vstemplate file exists on disk

--- template-checker.ps1
+++ template-checker.ps1
@@ -45,6 +45,7 @@ function ValidateBaseTemplateId(){
         foreach($baseTempId in $baseTemplateIdsFromTemplate){
             # check to see that there is a corresponding value for BaseTemplates.BaseTemplate.ID
             $foundBaseTempId = $template.TemplateDefinition.BaseTemplates.BaseTemplate.ID | Where-Object { [string]::Compare($_,$baseTempId,$true) -eq 0}
+            "Checking BaseTemplates.BaseTemplate.ID value for UI.Template.BaseTemplateID [{0}]" -f $baseTempId | Write-Verbose
             if(!$foundBaseTempId){
                 $errorFound = $true
                 $msg = ("Missing BaseTemplates.BaseTemplate.ID value for UI.Template.BaseTemplateID [{0}]" -f $baseTempId)
@@ -55,36 +56,72 @@ function ValidateBaseTemplateId(){
     }
     End{
         if($errorFound){ return $baseTempErrors }
-        else{ "no errors in ValidateBaseTemplateId" | Write-Host }
+        else{ "no errors in ValidateBaseTemplateId" | Write-Host -ForegroundColor Green}
         "ValidateBaseTemplateId finished" | Write-Verbose 
     }
 }
 function ValidateUnitTestIdFromUI(){
-    Param(
+    param(
         [Parameter(Mandatory=$true)]
         [xml]$template
     )
 
-    Begin{ "ValidateUnitTestIdFromUI starting" | Write-Verbose }
+    begin{ "ValidateUnitTestIdFromUI starting" | Write-Verbose }
     
-    Process{
+    process{
         $unitErrors = @()
         $errorsFound = $false
         $unitTestIdFromUI = $template.TemplateDefinition.UI.Template.UnitTest.DefaultBaseTemplateId
         foreach($unitTestId in $unitTestIdFromUI){
             $foundUnitTestId = $template.TemplateDefinition.BaseTemplates.BaseTemplate.ID | Where-Object { [string]::Compare($_,$unitTestId,$true) -eq 0}
+            "Checking UI.Template.UnitTest.DefaultBaseTemplateId [{0}]" -f $unitTestId | Write-Verbose
             if(!$foundUnitTestId){
                 $errorsFound = $true
-                $msg = ("Missing BaseTemplates.BaseTemplate.ID for UI.Template.BaseTemplateID [{0}]" -f $unitTestId)
+                $msg = ("Missing BaseTemplates.BaseTemplate.ID for UI.Template.UnitTest.DefaultBaseTemplateId [{0}]" -f $unitTestId)
                 $unitErrors += $msg
                 $msg | Write-Error
             }
         }
     }
 
-    End{
+    end{
         if($errorsFound){return $unitErrors}
-        else{ "no unit test errors found" | Write-Host }
+        else{ "no unit test errors found" | Write-Host -ForegroundColor Green}
+        "ValidateUnitTestIdFromUI finished" | Write-Verbose 
+    }
+}
+
+function ValidateVsTemplateExistsForBaseTemplate(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [xml]$template,
+        [Parameter(Mandatory=$true)]
+        [string]$templatePath
+    )
+    begin{ "ValidateUnitTestIdFromUI starting" | Write-Verbose }
+    process{
+        $tempErrors = @()
+        # check that the folder exists referred to by 
+        $baseTemplatePaths = $template.TemplateDefinition.BaseTemplates.BaseTemplate.VSTemplatePath
+        # remove duplicates
+        $baseTemplatePaths = ($baseTemplatePaths | select -Unique)
+        $templateDirectory = 
+        foreach($baseTempPath in $baseTemplatePaths){
+            # ' the file should be next to templates.xml in BaseTmplates\<PATH>
+            # Split('\')[1] at the end because the folder in the template path is evidenlty ignored
+            $expectedFolderPath = Join-Path -Path (Get-Item $templatePath).Directory.FullName -ChildPath ("BaseTemplates\{0}" -f $baseTempPath.Split('\')[1])
+            "Checking for .vstemplate file at [{0}]" -f $expectedFolderPath | Write-Verbose
+            if(!(Test-Path $expectedFolderPath)){
+                $errorsFound = $true
+                $msg = ("Expected to find .vstemplate file at [{0}] but it was not found" -f $expectedFolderPath)
+                $tempErrors += $msg
+                $msg | Write-Error
+            }
+        }
+    }
+    end{
+        if($errorsFound){return $tempErrors}
+        else{ "All .vstemplate files fond for BaseTemplates" | Write-Host -ForegroundColor Green }
         "ValidateUnitTestIdFromUI finished" | Write-Verbose 
     }
 }
@@ -109,7 +146,7 @@ if(!$isValid){
     "The file [{0}] is not valid based on the given xsd [{1}]" -f $templatePath, $templateXsdPath | Write-Verbose
     exit 1
 }
-else{ "The file is valid based on the xsd provided" | Write-Host }
+else{ "The file is valid based on the xsd provided" | Write-Host -ForegroundColor Green }
 
 $allErrors = @()
 [xml]$template = Get-Content $templatePath
@@ -120,11 +157,8 @@ $allErrors += $baseErrors
 $unitErrors = ValidateUnitTestIdFromUI -template $template
 $allErrors += $unitErrors
 
-
-
-
-
-
+$tempErrors = ValidateVsTemplateExistsForBaseTemplate -template $template -templatePath $templatePath
+$allErrors += $unitErrors
 
 
 
