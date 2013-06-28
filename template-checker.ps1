52ab9864cb80a4364a50955c8960344b127f363a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-06-28 12:03:06 -0700

    Updating to place test-xml in the script itself instead of a seperate module

--- template-checker.ps1
+++ template-checker.ps1
@@ -6,6 +6,46 @@
 )
 
 ######## Begin functions ######## 
+# taken from http://stackoverflow.com/a/16737772/105999
+# note this only works for XML files which do not have an xmlns declaration
+function Test-Xml {
+param(
+    [Parameter(ValueFromPipeline=$true)]
+    $xmlFile = $null,
+    $schemaFile = $null
+)
+
+BEGIN {
+    $failureMessages = ""
+    $script:isValid = $true
+}
+
+PROCESS {
+    $script:Context = New-Object -TypeName System.Management.Automation.PSObject
+    Add-Member -InputObject $Context -MemberType NoteProperty -Name Configuration -Value ""
+    $ConfigurationPath = $(Join-Path -Path $PWD -ChildPath "Configuration")
+
+    # Load xml and its schema
+    $Context.Configuration = [xml](Get-Content -LiteralPath $xmlFile)
+    $Context.Configuration.Schemas.Add($null, $schemaFile) | Out-Null    
+
+    Try{
+    $Context.Configuration.Validate(
+        {
+            $script:isValid = $false
+            $failureMessages +=  ("$($_.Message)" + [System.Environment]::NewLine)            
+            "ERROR: The XML file [$xmlFile] is not valid. $($_.Message)" | Write-Error
+        })
+    }
+    Catch{
+        [System.Exception]
+        $script:isValid = $false
+        $error
+    }
+}
+End{ return $script:isValid }
+
+}
 function IsTemplateXmlValid(){
     Param(
         [Parameter(Mandatory=$true)]
@@ -184,12 +224,12 @@ function ValidateReferencedRules(){
     }
 }
 ####### End functions ####### 
-if(Get-Module xml-helpers){
-    "Removing xml-helpers module so that it can be imported again" | Write-Verbose
-    Remove-Module xml-helpers
-}
+#if(Get-Module xml-helpers){
+#    "Removing xml-helpers module so that it can be imported again" | Write-Verbose
+#    Remove-Module xml-helpers
+#}
 # Importing modules
-Import-Module .\xml-helpers.psm1
+#Import-Module .\xml-helpers.psm1
 ################### Begin script ###################
 if(!(Test-Path $templatePath)){
     "Template file not found at [{0}]" -f $templatePath | Write-Error
@@ -220,7 +260,7 @@ if($allErrors.Count -gt 0){
 }
 
 
-Remove-Module xml-helpers
+#Remove-Module xml-helpers
 
 
 
